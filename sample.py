3d44552af042ebb697742d4250ac878bd1f6e864
gomachan46 <shiro.gomachan46@gmail.com>
2020-05-31 01:25:42 +0900

    とりあえず二値化してみる

--- sample.py
+++ sample.py
@@ -6,12 +6,13 @@ fmt = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
 frame_count = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
 fps = int(video.get(cv2.CAP_PROP_FPS))
 size = (int(video.get(cv2.CAP_PROP_FRAME_WIDTH)), int(video.get(cv2.CAP_PROP_FRAME_HEIGHT)))
-writer = cv2.VideoWriter('./output/sample.mp4', fmt, fps, size)
+writer = cv2.VideoWriter('./output/sample.mp4', fmt, fps, size, False)
 
 for i in range(frame_count):
     ret, frame = video.read()
-    writer.write(frame)
+    gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+    ret, threshold_image = cv2.threshold(gray, 225, 255, cv2.THRESH_BINARY)
+    writer.write(threshold_image)
 
 video.release()
 writer.release()
-cv2.destroyAllWindows()
