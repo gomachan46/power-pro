6b3b2c46a5af95e7bd57a9998d2777be4bbb8de8
gomachan46 <shiro.gomachan46@gmail.com>
2020-05-31 18:44:09 +0900

    雑にspeed_detectorも実装

--- sample.py
+++ sample.py
@@ -1,44 +1,18 @@
-from PIL import Image
-import numpy as np
 import cv2
-import pyocr
-import pyocr.builders
 from pitch_type_detector import PitchTypeDetector
-
-def cv2pil(image):
-    new_image = image.copy()
-    if new_image.ndim == 2:  # モノクロ
-        pass
-    elif new_image.shape[2] == 3:  # カラー
-        new_image = cv2.cvtColor(new_image, cv2.COLOR_BGR2RGB)
-    elif new_image.shape[2] == 4:  # 透過
-        new_image = cv2.cvtColor(new_image, cv2.COLOR_BGRA2RGBA)
-    new_image = Image.fromarray(new_image)
-    return new_image
-
-def image2text(image):
-    ocr_tools = pyocr.get_available_tools()
-    ocr_tool = ocr_tools[0]
-    return ocr_tool.image_to_string(cv2pil(image), lang="jpn", builder=pyocr.builders.TextBuilder(tesseract_layout=6))
-
+from speed_detector import SpeedDetector
 
 video = cv2.VideoCapture('./input/sample.mp4')
 fmt = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
 frame_count = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
-fps = int(video.get(cv2.CAP_PROP_FPS))
-width = 256
-height = 50
-pitch_type_params = { 'x': 512, 'y': 576 }
-speed_params = { 'x': 512, 'y': 626 }
+pitch_type_detector = PitchTypeDetector()
+speed_detector = SpeedDetector()
+
 pitch_type_text = ''
 speed_text = ''
-pitch_type_detector = PitchTypeDetector()
 
 for i in range(frame_count):
     _, frame = video.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-    _, threshold_image = cv2.threshold(gray, 215, 255, cv2.THRESH_BINARY)
-
     if pitch_type_text == '':
         ptt, pti = pitch_type_detector.detect(frame)
         if ptt != '':
@@ -46,11 +20,10 @@ for i in range(frame_count):
             cv2.imwrite('./output/pitch_type.jpg', pti)
 
     if speed_text == '':
-        speed_image = threshold_image[speed_params['y'] : speed_params['y'] + height, speed_params['x'] : speed_params['x'] + width]
-        st = image2text(speed_image)
+        st, si = speed_detector.detect(frame)
         if st != '':
             speed_text = st
-            cv2.imwrite('./output/speed.jpg', speed_image)
+            cv2.imwrite('./output/speed.jpg', si)
 
 video.release()
 print(f'{pitch_type_text}, {speed_text}')
